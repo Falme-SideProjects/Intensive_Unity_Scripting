cc4c9e0ea2cef35406de95086bb8d556154024b7
Falme Streamless <falme_shyness@hotmail.com>
2018-12-27 21:55:25 -0200

    Feito Exercicio Beginner 01

---
+++ Assets/Beginner_01/ChangeCubeColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCubeColor : MonoBehaviour {
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.R)){
+			GetComponent<MeshRenderer>().material.color = Color.red;
+		} else if(Input.GetKeyDown(KeyCode.G)){
+			GetComponent<MeshRenderer>().material.color = Color.green;
+		} else if(Input.GetKeyDown(KeyCode.B)){
+			GetComponent<MeshRenderer>().material.color = Color.blue;
+		}
+	}
+}
