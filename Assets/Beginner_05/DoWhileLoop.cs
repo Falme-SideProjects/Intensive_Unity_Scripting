2fbe2c442943e877aa9037a6d5f0e7d8189e1841
Falme Streamless <falme_shyness@hotmail.com>
2018-12-27 22:38:03 -0200

    Finalizado Beginner Activity 5

---
+++ Assets/Beginner_05/DoWhileLoop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoWhileLoop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		bool shouldContinue = false;
+
+		do{
+			print("Hello World");
+		} while (shouldContinue == true);
+		
+	}
+	
+}
---
+++ Assets/Beginner_05/WhileLoop.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoop : MonoBehaviour {
+
+	int numberOfCups = 4;
+
+
+	void Start () {
+		while(numberOfCups > 0){
+			Debug.Log("I've washed a cup");
+			numberOfCups--;
+		}
+	}
+	
+}
