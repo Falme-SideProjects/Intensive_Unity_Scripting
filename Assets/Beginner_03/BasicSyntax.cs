2ab9fe43c6261929a8120c4fe48b3de0ef6f0050
Falme Streamless <falme_shyness@hotmail.com>
2018-12-27 22:15:00 -0200

    Finalizado Beginner Activity 3

---
+++ Assets/Beginner_03/BasicSyntax.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicSyntax : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		//This line is there to tell me the X position
+
+		/*Hit There
+		 *this is two lines
+		 *
+		 *
+		 */
+
+		Debug.Log(transform.position.x);
+
+		if(transform.position.y <= 5f){
+			Debug.Log("I'm about to hit the ground");
+		}	
+	}
+	
+}
