2f2c86bc4311443444415652d7788ffde53baca4
Falme Streamless <falme_shyness@hotmail.com>
2018-12-27 22:23:58 -0200

    Finalizado Beginner Activity 4

---
+++ Assets/Beginner_04/IfStatements.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour {
+
+	public float coffeeTemperature = 85.0f;
+	public float hotLimitTemperature = 70.0f;
+	public float coldLimitTemperature = 40.0f;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest();
+
+		coffeeTemperature -= Time.deltaTime;
+	}
+
+	void TemperatureTest(){
+
+		// If the coffe temperature is greater than 70
+		if(coffeeTemperature > hotLimitTemperature){
+			print("Coffee is too hot!");
+		}
+		else if(coffeeTemperature < coldLimitTemperature){
+			print("Coffee is too cold");
+		}
+		else {
+			print(" Coffee is just right");
+		}
+
+	}
+}
