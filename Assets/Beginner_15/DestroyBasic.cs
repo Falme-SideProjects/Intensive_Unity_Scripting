b9923f102e51c7b7916db7ba5988ab804a887909
Falme Streamless <falme_shyness@hotmail.com>
2018-12-29 13:35:07 -0200

    Activity 15 finalizado

---
+++ Assets/Beginner_15/DestroyBasic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBasic : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Destroy(gameObject);
+		}
+	}
+}
