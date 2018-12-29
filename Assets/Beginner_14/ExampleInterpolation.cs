6184e2c13ae085d766266f2a79e892ea07f1ff70
Falme Streamless <falme_shyness@hotmail.com>
2018-12-29 13:25:39 -0200

    Activity 14 finalizado

---
+++ Assets/Beginner_14/ExampleInterpolation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleInterpolation : MonoBehaviour {
+
+	public Light light;
+
+	// Use this for initialization
+	void Start () {
+
+		float result;
+
+		result = Mathf.Lerp(3f, 5f, 0.5f);
+		Debug.Log("Between 3 an 5, with 0.5, is " + result);
+
+		Vector3 from = new Vector3 (1f, 2f, 3f);
+		Vector3 to = new Vector3(5f,6f,7f);
+
+		Vector3 vectorResult = Vector3.Lerp(from, to, 0.75f);
+		Debug.Log("About Vectors, Between 1,2,3 & 5,6,7 , with 0.75, is : "+ vectorResult);
+
+
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+	}
+}
