586913f93aa77a4c61e8f5eb46dcd564f22d259d
Falme Streamless <falme_shyness@hotmail.com>
2018-12-29 00:17:37 -0200

    Terminado Activity 7

---
+++ Assets/Beginner_07/AwakeAndStart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Start Called");
+	}
+	
+	void Awake(){
+		Debug.Log("Awake Called");
+	}
+}
