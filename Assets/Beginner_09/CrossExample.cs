37e8b3e4ecec708ab55940428a1ce1f2279c40ba
Falme Streamless <falme_shyness@hotmail.com>
2018-12-29 00:37:55 -0200

    Terminado Activity 9

---
+++ Assets/Beginner_09/CrossExample.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossExample : MonoBehaviour {
+
+	Vector3 GetNormal(Vector3 a,Vector3 b,Vector3 c){
+		Vector3 side1 = b- a;
+		Vector3 side2 = c- a;
+
+		return Vector3.Cross(side1, side2).normalized;
+	}
+}
---
+++ Assets/Beginner_09/DotExample.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotExample : MonoBehaviour {
+
+	public Transform other;
+
+	// Update is called once per frame
+	void Update () {
+		if(other){
+			Vector3 forwaerd = transform.TransformDirection(Vector3.forward);
+			Vector3 toOther = other.position - transform.position;
+
+			if(Vector3.Dot(forwaerd, toOther) < 0){
+				print("The other transform is behind me!!");
+			}
+		}
+	}
+}
