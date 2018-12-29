884dc9ee459e5816ce281214c33507320afd0cfe
Falme Streamless <falme_shyness@hotmail.com>
2018-12-29 12:42:29 -0200

    Activity 9 Melhor exemplificado

--- Assets/Beginner_09/CrossExample.cs
+++ Assets/Beginner_09/CrossExample.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrossExample : MonoBehaviour {
+public class CrossExample{
 
-	Vector3 GetNormal(Vector3 a,Vector3 b,Vector3 c){
+	public Vector3 GetNormal(Vector3 a,Vector3 b,Vector3 c){
 		Vector3 side1 = b- a;
 		Vector3 side2 = c- a;
 
--- Assets/Beginner_09/DotExample.cs
+++ Assets/Beginner_09/DotExample.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class DotExample : MonoBehaviour {
 
 	public Transform other;
+	public Transform andAnotherOther;
+
+	void Start(){
+		CrossExample _cx = new CrossExample();
+		Debug.Log(_cx.GetNormal(transform.position, other.position, andAnotherOther.position));
+	}
 
 	// Update is called once per frame
 	void Update () {
 		if(other){
 			Vector3 forwaerd = transform.TransformDirection(Vector3.forward);
 			Vector3 toOther = other.position - transform.position;
+
+			Debug.Log(Vector3.Dot(forwaerd, toOther));
 
 			if(Vector3.Dot(forwaerd, toOther) < 0){
 				print("The other transform is behind me!!");
