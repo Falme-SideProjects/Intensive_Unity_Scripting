ec23968060626e07f0936b5e505402316ec830e3
Falme Streamless <falme_shyness@hotmail.com>
2018-12-30 16:39:07 -0200

    Activity 17 finalizado

---
+++ Assets/Beginner_17/DualAxisExample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DualAxisExample : MonoBehaviour {
+
+	public float range;
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		float xPos = h*range;
+		float yPos = v*range;
+
+		Debug.Log("Horizontal Value Returned: "+h.ToString("F2"));
+		Debug.Log("Vertical Value Returned: "+v.ToString("F2"));
+	}
+}
