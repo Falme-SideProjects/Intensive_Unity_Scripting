95208091f4b2c1938f7dea4777d2359eece28486
Falme Streamless <falme_shyness@hotmail.com>
2018-12-30 16:47:33 -0200

    Activity 18 finalizado

---
+++ Assets/Beginner_18/MouseClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour {
+
+	void OnMouseDown()
+	{
+		Rigidbody _rb = GetComponent<Rigidbody>();
+		_rb.AddForce(-transform.forward);
+		_rb.useGravity = true;
+	}
+}
