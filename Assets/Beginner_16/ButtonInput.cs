002e058614419fffff8b66b65cff8bd4903357fc
Falme Streamless <falme_shyness@hotmail.com>
2018-12-30 16:29:12 -0200

    Activity 16 finalizado

---
+++ Assets/Beginner_16/ButtonInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInput : MonoBehaviour {
+
+	public GUITexture graphics;
+	public Texture2D standard;
+	public Texture2D downgfx;
+	public Texture2D upgfx;
+	public Texture2D heldgfx;
+
+
+	// Use this for initialization
+	void Start () {
+		graphics.texture = standard;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool down = Input.GetButtonDown("Jump");
+		bool held = Input.GetButton("Jump");
+		bool up = Input.GetButtonUp("Jump");
+
+		if(down){
+			graphics.texture = downgfx;
+		}
+		else if(held){
+			graphics.texture = heldgfx;
+		}
+		else if(up){
+			graphics.texture = upgfx;
+		}
+		else{
+			graphics.texture = standard;
+		}
+
+		Debug.Log(" "+down+ "\n" + held + "\n" + up);
+
+	}
+}
---
+++ Assets/Beginner_16/KeyInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInput : MonoBehaviour {
+
+	public GUITexture graphics;
+	public Texture2D standard;
+	public Texture2D downgfx;
+	public Texture2D upgfx;
+	public Texture2D heldgfx;
+
+
+	// Use this for initialization
+	void Start () {
+		graphics.texture = standard;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool down = Input.GetKeyDown(KeyCode.Space);
+		bool held = Input.GetKey(KeyCode.Space);
+		bool up = Input.GetKeyUp(KeyCode.Space);
+
+		if(down){
+			graphics.texture = downgfx;
+		}
+		else if(held){
+			graphics.texture = heldgfx;
+		}
+		else if(up){
+			graphics.texture = upgfx;
+		}
+		else{
+			graphics.texture = standard;
+		}
+
+		Debug.Log(" "+down+ "\n" + held + "\n" + up);
+
+	}
+}
