0d404fd18f527b8e719372bfa0f90ce69e03596e
Falme Streamless <falme_shyness@hotmail.com>
2018-12-30 17:04:20 -0200

    Activity 19 finalizado

---
+++ Assets/Beginner_19/UsingOtherComponents.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingOtherComponents : MonoBehaviour {
+
+	public GameObject otherGameObject;
+
+
+	private ComponentAnotherScript anotherScript;
+	private YetAnotherScript yetAnotherScript;
+	private BoxCollider boxCol;
+
+
+	void Awake(){
+		anotherScript = GetComponent<ComponentAnotherScript>();
+		yetAnotherScript = otherGameObject.GetComponent<YetAnotherScript>();
+		boxCol = otherGameObject.GetComponent<BoxCollider>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		boxCol.size = new Vector3(3,3,3);
+		Debug.Log("The player's Score is "+anotherScript.playerScore);
+		Debug.Log("The player has died "+yetAnotherScript.numberOfPlayerDeaths + " times");
+	}
+	
+}
