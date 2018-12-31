97e523a6c6134fb2d9b8727f2f036a4ff2e4bcd0
Falme Streamless <falme_shyness@hotmail.com>
2018-12-30 22:26:06 -0200

    Activity 24 finalizado

---
+++ Assets/Beginner_24/ExamplesArrays.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExamplesArrays : MonoBehaviour {
+
+	public GameObject[] players;
+
+	// Use this for initialization
+	void Start () {
+		players = GameObject.FindGameObjectsWithTag("Player");
+
+		for(int i=0; i<players.Length; i++){
+			Debug.Log("Player Number "+i+" is named "+players[i].name);
+		}
+	}
+	
+}
