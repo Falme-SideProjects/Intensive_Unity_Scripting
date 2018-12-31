08e69a1652ee10529a25ac8b6741e8d85cf778b8
Falme Streamless <falme_shyness@hotmail.com>
2018-12-31 00:59:35 -0200

    Activity 04 finalizado

---
+++ Assets/Intermediate_04/OverloadSomeClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverloadSomeClass {
+
+	public int Add(int num1, int num2){
+		return num1+num2;
+	}
+
+	public string Add(string str1, string str2){
+		return str1+str2;
+	}
+}
---
+++ Assets/Intermediate_04/OverloadSomeOtherClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverloadSomeOtherClass : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		OverloadSomeClass myClass = new OverloadSomeClass();
+
+		myClass.Add(1,2);
+		myClass.Add("Hello", "World");
+	}
+	
+}
