eb8d8b27cf2d82cc90651326b2329a5b3c130906
Falme Streamless <falme_shyness@hotmail.com>
2018-12-31 15:46:14 -0200

    Activity 11 finalizado

---
+++ Assets/Intermediate_11/ExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods {
+
+	// Use this for initialization
+	public static void resetTransformation(this Transform trans){
+		trans.position = Vector3.zero;
+		trans.localRotation = Quaternion.identity;
+		trans.localScale = new Vector3(1,1,1);
+	}
+}
---
+++ Assets/Intermediate_11/ExtensionSomeClass.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtensionSomeClass : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.resetTransformation();
+	}
+	
+}
