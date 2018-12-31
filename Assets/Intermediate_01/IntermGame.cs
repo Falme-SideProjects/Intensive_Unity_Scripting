5ae2efe57ab11b7073041204b8aba2748aed4473
Falme Streamless <falme_shyness@hotmail.com>
2018-12-31 00:32:19 -0200

    Activity 01 finalizado

---
+++ Assets/Intermediate_01/IntermGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntermGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		IntermPlayer myPlayer = new IntermPlayer();
+
+		myPlayer.Experience = 5;
+		int x = myPlayer.Experience;
+		Debug.Log(x);
+
+	}
+	
+}
---
+++ Assets/Intermediate_01/IntermPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntermPlayer {
+
+	private int experience;
+
+	public int Experience{
+		get{
+			return this.experience;
+		}
+		set{
+			experience = value;
+		}
+	}
+
+	public int Level
+	{
+		get{
+			return experience/1000;
+		}
+		set{
+			experience = value * 1000;
+		}
+	}
+
+	public int Health{get; set;}
+
+}
