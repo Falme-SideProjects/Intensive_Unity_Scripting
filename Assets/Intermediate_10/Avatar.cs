36e29d9efa9da890e63b495a073e63908073c260
Falme Streamless <falme_shyness@hotmail.com>
2018-12-31 15:37:22 -0200

    Activity 10 finalizado

---
+++ Assets/Intermediate_10/Avatar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avatar : MonoBehaviour, IKillable, IDamageable<float> {
+    public void Damage(float damageTaken)
+    {
+       
+    }
+
+    public void Kill()
+    {
+        
+    }
+
+}
