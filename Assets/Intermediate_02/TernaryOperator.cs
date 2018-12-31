c8211ee1d143bb079deda4cfd7c2938f3343f840
Falme Streamless <falme_shyness@hotmail.com>
2018-12-31 00:38:00 -0200

    Activity 02 finalizado

---
+++ Assets/Intermediate_02/TernaryOperator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TernaryOperator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int health = 10;
+		string message;
+
+		message = health > 0 ? "Player is Alive!!":"Player is Dead";
+	}
+	
+}
