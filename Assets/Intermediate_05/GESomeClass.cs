4522168cb0928d2c4acbc28aa26f0801ad2b81b8
Falme Streamless <falme_shyness@hotmail.com>
2018-12-31 12:58:52 -0200

    Activity 05 finalizado

---
+++ Assets/Intermediate_05/GESomeClass.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GESomeClass {
+
+	public T GenericMethod<T>(T param){
+		return param;
+	}
+}
---
+++ Assets/Intermediate_05/GESomeOtherClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GESomeOtherClass : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GESomeClass myClass = new GESomeClass();
+
+		myClass.GenericMethod<int>(5);
+		myClass.GenericMethod<GenericClass<int>>(new GenericClass<int>());
+	}
+	
+}
