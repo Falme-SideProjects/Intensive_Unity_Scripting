29f1551161c4e776af67527316b63709ac239e6e
Falme Streamless <falme_shyness@hotmail.com>
2019-01-01 14:07:23 -0200

    Activity 16 finalizado

---
+++ Assets/Intermediate_16/DelegateScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateScript : MonoBehaviour {
+
+	delegate void MyDelegate(int num);
+	MyDelegate myDelegate;
+
+
+	// Use this for initialization
+	void Start () {
+		myDelegate = PrintNum;
+		myDelegate(50);
+
+		myDelegate = DoubleNum;
+		myDelegate(12);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void PrintNum(int num){
+		print("Print Num: "+ num);
+	}
+
+	void DoubleNum(int num){
+		print("Double Num: "+ num*2);
+	}
+}
---
+++ Assets/Intermediate_16/MulticastScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MulticastScript : MonoBehaviour {
+
+	delegate void MultiDelegate();
+	MultiDelegate myMultiDelegate;
+
+	// Use this for initialization
+	void Start () 
+	{
+		myMultiDelegate += PowerUp;
+		myMultiDelegate += TurnRed;
+
+		if(myMultiDelegate != null){
+			myMultiDelegate();
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void PowerUp()
+	{
+		print("Orb is powering up");
+	}
+
+	void TurnRed(){
+		GetComponent<MeshRenderer>().material.color = Color.red;
+	}
+}
