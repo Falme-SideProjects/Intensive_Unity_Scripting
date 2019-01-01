392b87e4b678b457fdb30f98a8d59b1e3f39f05b
Falme Streamless <falme_shyness@hotmail.com>
2019-01-01 16:50:00 -0200

    Activity 01 finalizado

---
+++ Assets/ECS_01/ECSRotator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class ECSRotator : MonoBehaviour {
+
+	public float speed;
+}
+
+/* public class ECSRotatorSystem : ComponentSystem
+{
+	struct Components
+	{
+		public ECSRotator rotator;
+		public Transform transform;
+	}
+
+	protected override void OnUpdate(){
+
+	}
+} */
+
+class ECSRotatorSystem : ComponentSystem
+{
+	float deltaTime = Time.deltaTime;
+
+	struct Components
+	{
+		public ECSRotator rotator;
+		public Transform transform;
+	}
+
+	protected override void OnUpdate()
+	{
+		foreach(var e in GetEntities<Components>())
+		{
+			e.transform.Rotate(0f, e.rotator.speed * deltaTime, 1f);
+		}
+	}
+}
