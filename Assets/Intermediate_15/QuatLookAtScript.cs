5ec453ac1d1cdf6ddf8cf9c90a2a3f06ae0b7642
Falme Streamless <falme_shyness@hotmail.com>
2019-01-01 03:00:11 -0200

    Activity 15 finalizado

---
+++ Assets/Intermediate_15/QuatLookAtScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuatLookAtScript : MonoBehaviour {
+
+	public Transform target;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 relativePos = target.position - transform.position;
+		transform.rotation = Quaternion.LookRotation(relativePos);
+	}
+}
