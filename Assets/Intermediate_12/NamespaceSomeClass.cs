8dd7f45edee4c0da0eedf0bc95eb2972de38425b
Falme Streamless <falme_shyness@hotmail.com>
2019-01-01 00:42:38 -0200

    Activity 12 finalizado

---
+++ Assets/Intermediate_12/NamespaceSomeClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Intermediate_12
+{
+	public class NamespaceSomeClass : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+	}
+}
