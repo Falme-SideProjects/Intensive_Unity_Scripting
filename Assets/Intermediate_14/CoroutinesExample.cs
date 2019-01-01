d9a2a191452b927de20ac09693ff6293721c3e0f
Falme Streamless <falme_shyness@hotmail.com>
2019-01-01 01:34:09 -0200

    Activity 14 finalizado

---
+++ Assets/Intermediate_14/CoroutinesExample.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutinesExample : MonoBehaviour {
+
+	public float smoothing = 1f;
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(MyCoroutine(target));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator MyCoroutine(Transform target){
+
+		while(Vector3.Distance(transform.position, target.position) > 0.05f){
+			transform.position = Vector2.Lerp(transform.position, target.position, smoothing * Time.deltaTime);
+
+			yield return null;
+		}
+
+		print("Reached the Target");
+
+		yield return new WaitForSeconds(3f);
+
+		print("MyCoroutine is now finished");
+
+	}
+}
