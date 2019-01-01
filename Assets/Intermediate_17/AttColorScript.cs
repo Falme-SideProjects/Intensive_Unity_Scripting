e67fde3b460596907404706d860beb25c6af9b74
Falme Streamless <falme_shyness@hotmail.com>
2019-01-01 14:19:31 -0200

    Activity 17 finalizado

---
+++ Assets/Intermediate_17/AttColorScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AttColorScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<MeshRenderer>().sharedMaterial.color = Color.blue;
+	}
+	
+}
---
+++ Assets/Intermediate_17/AttSpinScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttSpinScript : MonoBehaviour {
+
+	[Range(-100,100)] public int speed = 0;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(0, speed * Time.deltaTime, 0));
+	}
+}
